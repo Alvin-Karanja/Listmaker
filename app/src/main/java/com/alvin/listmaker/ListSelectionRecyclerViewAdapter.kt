6530b16b80596b5f7418c8567c956a405590cb36
Alvin-Karanja <alvin2@protonmail.com>
2020-09-30 11:09:23 +0300

    Used LayoutInflater object to use the ViewHolder

--- app/src/main/java/com/alvin/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/alvin/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.alvin.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,7 +9,17 @@ class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewH
     val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("Not yet implemented")
+
+        // LayoutInflater is a system utility used to instantiate (or "inflate") a
+        // layout XML file into its corresponding View objects.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_selection_view_holder,
+                parent,
+                false)
+
+        // A ListSelectionViewHolder object is created, passing in the view created from
+        //the layout
+        return ListSelectionViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
