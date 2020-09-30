ba9b8ee9a97166945fa63eb725495043994bea25
Alvin-Karanja <alvin2@protonmail.com>
2020-09-30 12:08:06 +0300

    Added a Primary Constructor

---
+++ app/src/main/java/com/alvin/listmaker/ListDataManager.kt
@@ -0,0 +1,12 @@
+package com.alvin.listmaker
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+
+class ListDataManager(private val context: Context) {
+    fun saveList(list: TaskList) {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context).edit()
+        sharedPreferences.putStringSet(list.name, list.tasks.toHashSet())
+        sharedPreferences.apply()
+    }
+}
