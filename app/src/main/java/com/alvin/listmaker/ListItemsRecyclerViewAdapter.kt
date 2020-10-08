cb43480b665cbfa5b49cb592d16cd3fe76e51908
Alvin-Karanja <alvin2@protonmail.com>
2020-10-08 10:30:56 +0300

    method tells the RecyclerView how many items to display

--- app/src/main/java/com/alvin/listmaker/ListItemsRecyclerViewAdapter.kt
+++ app/src/main/java/com/alvin/listmaker/ListItemsRecyclerViewAdapter.kt
@@ -1,6 +1,18 @@
 package com.alvin.listmaker
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListItemsRecyclerViewAdapter(var list: TaskList) : RecyclerView.Adapter<ListItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return list.tasks.size
+    }
 }
